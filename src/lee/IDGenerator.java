fa281d51e99fc01521bfc4f1ca2a28afee63f37a
wubo <wubo@ppdai.com>
2016-08-08 18:45:05 +0800

    解决多线程问题

--- src/lee/IDGenerator.java
+++ src/lee/IDGenerator.java
@@ -1,18 +1,10 @@
 package lee;
 
 import java.io.Serializable;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 import org.hibernate.HibernateException;
-import org.hibernate.SQLQuery;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
 import org.hibernate.engine.SessionImplementor;
 import org.hibernate.id.IdentifierGenerator;
-import org.hibernate.persister.entity.AbstractEntityPersister;
 /**
  * 
  * @GeneratedValue(generator = "system-uuid") 
@@ -26,51 +18,10 @@ import org.hibernate.persister.entity.AbstractEntityPersister;
  */
 public class IDGenerator implements IdentifierGenerator{
 
-	/**
-	 * 存放不同的序列对应值
-	 */
-	private Map<String, Integer> map = new ConcurrentHashMap<String, Integer>();
-
-	/**
-	 * 缓存长度
-	 */
-	private static String seqCacheSuffix = "_SEQCACHE";
-
-	/**
-	 * 序列值
-	 */
-	private static String sequenceSuffix = "_SEQUENCE";
-
 	@Override
 	public Serializable generate(SessionImplementor arg0, Object arg1) throws HibernateException {
-		Session session = arg0.getFactory().openSession();
-		Transaction tx = session.beginTransaction();
-		//持久化对象  
-		AbstractEntityPersister classMetadata =  (AbstractEntityPersister) arg0.getFactory().getClassMetadata(arg1.getClass());
-		String tableName = classMetadata.getTableName();//表名
-		//缓存长度
-		Integer seqCache = map.get(tableName + seqCacheSuffix);
-		//序列值
-		Integer sequence = map.get(tableName + sequenceSuffix);
-		if (seqCache == null){
-			SQLQuery query = session.createSQLQuery("SELECT mycat_seq_nextval('"+tableName +"')");
-			String seq = (String) query.uniqueResult();
-			String[] seqs = seq.split(",");
-			//获取缓存长度
-			seqCache = Integer.valueOf(seqs[1]);
-			//获取此序列初始值
-			sequence = Integer.valueOf(seqs[0]);
-		} 
-		SimpleDateFormat formatter = new SimpleDateFormat ("yyyyMMddHHmmss");
-		String id = formatter.format(new Date()) + "" + sequence;
-		System.out.println(id);
-		seqCache--;
-		sequence++;
-		map.put(tableName + seqCacheSuffix, seqCache);
-		map.put(tableName + sequenceSuffix, sequence);
-		tx.commit();
-		session.close();
-		return Long.valueOf(id);
+		KeySingleton key = KeySingleton.getInstance();
+		return key.sequence(arg0, arg1);
 
 	}
 
